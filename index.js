8566f3f43514f2a05ed30211241a0588377d6b2d
SergeyKuleshov <kilinserg89@mail.ru>
2022-06-25 10:03:27 +0300

    Fix index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import _ from "lodash";
 
-console.log('Hello, Hexlet');
-console.log(_.last(['one', 'two']));
+console.log("Hello, Hexlet");
+console.log(_.last(["one", "two"]));
+
+function a10(a) {
+  if (a > 5) {
+    return console.log(a + 10);
+  }
+}
+a10(6);
